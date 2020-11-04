f4c4b7831bdd1b2aae1c4651775f54b38b20fb06
Quentin Vignier <qvignier@igraal.com>
2020-11-04 14:03:48 +0100

    Use BaseDTOInterface for signature

---
+++ src/Common/DTO/AbstractSQLBaseDTOProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace COL\Library\Infrastructure\Common\DTO;
+
+use COL\Library\Infrastructure\Common\Repository\AbstractSQLBaseRepository;
+
+abstract class AbstractSQLBaseDTOProvider
+{
+    protected AbstractSQLBaseRepository $repository;
+
+    /**
+     * @return BaseDTOInterface[]
+     */
+    public function getManyByCriteria(array $critieria = [], array $selects = [], array $orders = [], ?int $limit = null, ?int $offset = null): array
+    {
+        return $this->getRepository()->findManyByCriteria($critieria, $selects, $orders, $limit, $offset);
+    }
+
+    /**
+     * @return BaseDTOInterface|null
+     */
+    public function getOneByCriteria(array $critieria, array $selects = []): ?BaseDTOInterface
+    {
+        return $this->getRepository()->findOneByCriteria($critieria, $selects);
+    }
+
+    public function exists(array $critieria): bool
+    {
+        //return $this->getRepository()->exists($critieria);
+    }
+
+    protected function getRepository(): AbstractSQLBaseRepository
+    {
+        return $this->repository;
+    }
+}
