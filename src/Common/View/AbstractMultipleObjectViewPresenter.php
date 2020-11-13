570632d53d1c83a1dae6e2030b9a2829e0ff90ad
Quentin Vignier <qvignier@igraal.com>
2020-11-13 16:09:32 +0100

    Move buildMultipleObjectVueModel to abstract class as the work is always the same

--- src/Common/View/AbstractMultipleObjectViewPresenter.php
+++ src/Common/View/AbstractMultipleObjectViewPresenter.php
@@ -2,8 +2,32 @@
 
 namespace COL\Library\Infrastructure\Common\View;
 
+use COL\Library\Contracts\View\Model\BaseViewModelInterface;
+use COL\Library\Infrastructure\Common\DTO\BaseDTOInterface;
+
 abstract class AbstractMultipleObjectViewPresenter implements MultipleObjectViewPresenterInterface
 {
+    /**
+     * @param BaseDTOInterface[] $dtos
+     *
+     * @return BaseViewModelInterface[]
+     */
+    public function buildMultipleObjectVueModel(array $dtos, string $displayFormat, ?int $nbTotal = null, ?int $pageNumber = null, ?int $nbPerPage = null): array
+    {
+        $models = [];
+        foreach ($dtos as $dto) {
+            if (self::DISPLAY_FORMAT_LARGE === $displayFormat) {
+                $models[] = $this->buildVueModelLargeFormat($dto);
+            } elseif(self::DISPLAY_FORMAT_MEDIUM === $displayFormat) {
+                $models[] = $this->buildVueModelMediumFormat($dto);
+            } else {
+                $models[] = $this->buildVueModelSmallFormat($dto);
+            }
+        }
+
+        return $this->formatWithPagination($models, $nbTotal, $pageNumber, $nbPerPage);
+    }
+
     protected function formatWithPagination(array $models, ?int $nbTotal, ?int $pageNumber, ?int $nbPerPage): array
     {
         $lastPage = floor($nbTotal / $nbPerPage);
