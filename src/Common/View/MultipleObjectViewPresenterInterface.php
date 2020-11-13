fa26849c1b112cc67c2d66b6bd79b2ac152aaf04
Quentin Vignier <qvignier@igraal.com>
2020-11-13 16:04:42 +0100

    Handle 3 differents types of format for view model construction (small, medium, large)

--- src/Common/View/MultipleObjectViewPresenterInterface.php
+++ src/Common/View/MultipleObjectViewPresenterInterface.php
@@ -7,10 +7,26 @@ use COL\Library\Infrastructure\Common\DTO\BaseDTOInterface;
 
 interface MultipleObjectViewPresenterInterface
 {
+    public const DISPLAY_FORMAT_SMALL = 'small';
+    public const DISPLAY_FORMAT_MEDIUM = 'small';
+    public const DISPLAY_FORMAT_LARGE = 'small';
+
     /**
      * @param BaseDTOInterface[] $dtos
      *
      * @return BaseViewModelInterface[]
      */
-    public function buildMultipleObjectVueModel(array $dtos, ?int $nbTotal = null, ?int $pageNumber = null, ?int $nbPerPage = null): array;
+    public function buildMultipleObjectVueModel(
+        array $dtos,
+        string $displayFormat,
+        ?int $nbTotal = null,
+        ?int $pageNumber = null,
+        ?int $nbPerPage = null
+    ): array;
+
+    public function buildVueModelSmallFormat(BaseDTOInterface $baseDTO): BaseViewModelInterface;
+
+    public function buildVueModelMediumFormat(BaseDTOInterface $baseDTO): BaseViewModelInterface;
+
+    public function buildVueModelLargeFormat(BaseDTOInterface $baseDTO): BaseViewModelInterface;
 }
